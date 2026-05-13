73adb020c40a6222853c07e7343f69330498bb5f
Clinton-odia <odiamehiclinton@gmail.com>
2026-05-13 05:43:09 +0100

    updated meals and component

--- code/02-nextjs-essentials/05-onwards-foodies-starting-project/app/meals/page.js
+++ code/02-nextjs-essentials/05-onwards-foodies-starting-project/app/meals/page.js
@@ -1,3 +1,25 @@
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/component/meals/MealGrid';
 export default function MealsPage() {
-  return <h1>Meals</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          {' '}
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <div className={classes.cta}>
+          <Link href='/meals/share'>Share Your Favorite Recipe</Link>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
